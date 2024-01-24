36a9830e4e8b242e6cf530106ed1f7f380d5eb9f
SergeyPavluchenko <102330212+SergeyPavluchenko@users.noreply.github.com>
2024-01-24 21:29:43 +0000

    Deploying to gh-pages from @ SergeyPavluchenko/goit-js-hw-10_test@abec1a768186faa6c61deda76fd404d9916ab176 🚀

--- index.ec22419c.js
+++
@@ -1,2 +0,0 @@
-!function(){var r;API_KEY="live_rnnYDp9s8iR62R9IizxA0JiibxhK2dxyt24ridFGGcAZS6MkHaeULrqjFpOhJr5E",r={headers:{"x-api-key":API_KEY}},fetch("".concat("https://api.thecatapi.com/v1","/breeds"),r).then((function(r){if(console.log(r),!r.ok)throw new Error(r.statusText);return r.json()})).catch((function(r){return console.error(r)}))}();
-//# sourceMappingURL=index.ec22419c.js.map
